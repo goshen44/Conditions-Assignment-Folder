8a03a71bbd6fe2b6b2e2fd97793161dd2c2fe1e5
goshen44 <jpgoshen@gmail.com>
2015-07-23 21:19:37 -0400

    Movie Times Commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+/*
+    Joshuah Goshen
+    SDI 1507
+    July 23, 2015
+    Conditional Assignment
+ */
+
+// I am going to the movies and these are My Variables that I am going to use
+var firstStep   = "I am going to get in the car and drive to the movie theatre"; //The first step in getting to the movies
+var secondStep  = "Then I am going get out of the car to walk"; //The second step in getting into the movies
+var movie       = "Mad Max"; //Is the name of the movie I'm going to see
+var movieTime   = "7:50"; //Time the movie is playing that I am going to
+var altMovieTime= "8:30"; //The other movie time
+
+console.log("In order to get to the movies, first" + " " + firstStep + ".");
+console.log(secondStep + "up to the ticket window" + ".");
+console.log("I'm going to see the movie" + " " + movie + ".");
+console.log("There are two movie times" + " " + movieTime + " " + "and" + " " + altMovieTime + "." );
+
+
+
+
+
